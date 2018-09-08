bd562879cb96db8e79ded6515d4018e58efea83f
Zhou Qingwu <zhqingwu@gmail.com>
2018-09-08 08:48:04 +0800

    error

--- app/Http/Controllers/Pay/AlipayController.php
+++ app/Http/Controllers/Pay/AlipayController.php
@@ -6,12 +6,15 @@ use App\Http\Controllers\Controller;
 use App\Models\Good\Order;
 use App\Models\User\Recharge;
 use App\Models\User\User;
+use App\Models\Common\Pay;
+use Omnipay\Omnipay;
 use Illuminate\Http\Request;
 use Storage;
 
 class AlipayController extends Controller
 {
     public $gateway;
+
     public function __construct()
     {
         $pay = Pay::findOrFail(1);
@@ -22,8 +25,8 @@ class AlipayController extends Controller
         $this->gateway->setKey($set->alipay_key); //For MD5 sign type
         //$this->gateway->setPrivateKey('the_rsa_sign_key'); //For RSA sign type
         //$this->gateway->setAlipayPublicKey('the_alipay_public_key'); //For RSA sign type
-        $this->gateway->setReturnUrl(config('app.url').'/alipay/return');
-        $this->gateway->setNotifyUrl(config('app.url').'/alipay/gateway');
+        $this->gateway->setReturnUrl(config('app.url') . '/alipay/return');
+        $this->gateway->setNotifyUrl(config('app.url') . '/alipay/gateway');
     }
 
     // 异步&&同步通知页面，放一起了
@@ -36,29 +39,27 @@ class AlipayController extends Controller
         try {
             $response = $request->send();
             $resData = $response->getData();
-            if($response->isPaid()){
+            if ($response->isPaid()) {
                 $oid = $resData['out_trade_no'];
-                $order = Order::where('order_id',$oid)->first();
+                $order = Order::where('order_id', $oid)->first();
                 if (!is_null($order) && $order->paystatus == 0) {
-                    $updateOrder = OrderApi::updateOrder($order,$paymod = '支付宝');
+                    $updateOrder = OrderApi::updateOrder($order, $paymod = '支付宝');
                     if (!$updateOrder) {
                         DB::rollback();
                         die('fail');
                     }
-                }
-                else
-                {
+                } else {
                     DB::rollback();
                     die('fail');
                 }
-                Storage::disk('log')->prepend('alipay.log',json_encode($resData));
+                Storage::disk('log')->prepend('alipay.log', json_encode($resData));
                 DB::commit();
                 die('success'); //The notify response should be 'success' only
-            }else{
+            } else {
                 /**
                  * Payment is not successful
                  */
-                Storage::disk('log')->prepend('alipay.log',json_encode($resData));
+                Storage::disk('log')->prepend('alipay.log', json_encode($resData));
                 DB::rollback();
                 die('fail'); //The notify response
             }
@@ -67,7 +68,7 @@ class AlipayController extends Controller
             /**
              * Payment is not successful
              */
-            Storage::disk('log')->prepend('alipay.log',$e->getMessage());
+            Storage::disk('log')->prepend('alipay.log', $e->getMessage());
             die('fail'); //The notify response
         }
     }
@@ -82,28 +83,28 @@ class AlipayController extends Controller
         try {
             $response = $request->send();
             $resData = $response->getData();
-            if($response->isPaid()){
+            if ($response->isPaid()) {
                 /**
                  * Payment is successful
                  */
                 // 消费记录
                 $oid = $resData['out_trade_no'];
-                $order = Recharge::where('order_id',$oid)->first();
+                $order = Recharge::where('order_id', $oid)->first();
                 // 支付成功，并且没有改状态时，给会员增加余额
                 if ($order->paystatus == 0) {
-                    User::where('id',$order->user_id)->increment('user_money',$order->money);
-                    Recharge::where('id',$order->id)->update(['paystatus'=>1,'paymod'=>'支付宝','paytime'=>date('Y-m-d H:i:s')]);
+                    User::where('id', $order->user_id)->increment('user_money', $order->money);
+                    Recharge::where('id', $order->id)->update(['paystatus' => 1, 'paymod' => '支付宝', 'paytime' => date('Y-m-d H:i:s')]);
                     // 消费记录
-                    app('com')->consume($order->user_id,$order->id,$order->money,'支付宝充值支付订单（'.$order->order_id.'）',1);
+                    app('com')->consume($order->user_id, $order->id, $order->money, '支付宝充值支付订单（' . $order->order_id . '）', 1);
                 }
                 DB::commit();
-                Storage::disk('log')->prepend('alipay.log',json_encode($resData));
+                Storage::disk('log')->prepend('alipay.log', json_encode($resData));
                 die('success'); //The notify response should be 'success' only
-            }else{
+            } else {
                 /**
                  * Payment is not successful
                  */
-                Storage::disk('log')->prepend('alipay.log',json_encode($resData));
+                Storage::disk('log')->prepend('alipay.log', json_encode($resData));
                 die('fail'); //The notify response
             }
         } catch (\Throwable $e) {
@@ -111,7 +112,7 @@ class AlipayController extends Controller
              * Payment is not successful
              */
             DB::rollback();
-            Storage::disk('log')->prepend('alipay.log',$e->getMessage());
+            Storage::disk('log')->prepend('alipay.log', $e->getMessage());
             die('fail'); //The notify response
         }
     }
